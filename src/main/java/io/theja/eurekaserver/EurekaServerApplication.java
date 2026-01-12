0f77335ccbb058a779a281af5dff9dd1a289e921
ksrntheja <srntkolla@gmail.com>
2026-01-12 13:30:22 +0530

    Adding eureka-server-demo project.

---
+++ src/main/java/io/theja/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package io.theja.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// TODO 3: Add annotation @EnableEurekaServer.
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
+
+/*
+TODO 4:
+
+http://localhost:8761/
+
+ */
+
---
+++ src/test/java/io/theja/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.theja.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
